62b9c98938ef9cc1f1db382752f001f1395698ac
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-01-05 16:31:50 +0100

    WIP

--- DS3TexUpUI/HeightMapGenerator.cs
+++ DS3TexUpUI/HeightMapGenerator.cs
@@ -5,13 +5,13 @@ namespace DS3TexUpUI
 {
     public static class HeightMapGenerator
     {
-        private static Slope[] ToSlopes(DS3NormalMap.NormalView normals)
+        public static ArrayTextureMap<Slope> ToSlopes(this DS3NormalMap.NormalView normals)
         {
             var count = normals.Count;
             var slopes = new Slope[count];
             for (int i = 0; i < count; i++)
                 slopes[i] = (Slope)normals[i];
-            return slopes;
+            return slopes.AsTextureMap(normals.Width);
         }
 
         public static ArrayTextureMap<float> Gen1<M>(M map)
@@ -34,6 +34,84 @@ namespace DS3TexUpUI
             result.Normalize();
             return result;
         }
+
+        public static ArrayTextureMap<float> Gen2(this DS3NormalMap.NormalView normals)
+        {
+            var slopes = ToSlopes(normals);
+
+            // var heights = GenerateHeightMapCross(slopes, normals.Width / 2, normals.Height / 2);
+            var heights = GenerateHeightMapCrossTopLeft(slopes);
+
+            heights.Normalize();
+            return heights;
+        }
+        public static ArrayTextureMap<float> GenerateHeightMapCrossTopLeft(ArrayTextureMap<Slope> slopes)
+        {
+            static float GetDX(ArrayTextureMap<Slope> slopes, int y, int x1, int x2)
+            {
+                var stride = y * slopes.Width;
+                return (slopes[stride + x1].dx + slopes[stride + x2].dx) * 0.5f;
+            }
+            static float GetDY(ArrayTextureMap<Slope> slopes, int x, int y1, int y2)
+            {
+                return (slopes[y1 * slopes.Width + x].dy + slopes[y2 * slopes.Width + x].dy) * 0.5f;
+            }
+
+            var w = slopes.Width;
+            var h = slopes.Height;
+
+            var result = new float[slopes.Count].AsTextureMap(slopes.Width);
+
+            // Draw the cross
+            for (var x = 1; x < w; x++)
+                result[x, 0] = result[x - 1, 0] + GetDX(slopes, 0, x - 1, x);
+            for (var y = 0 + 1; y < h; y++)
+                result[0, y] = result[0, y - 1] + GetDY(slopes, 0, y - 1, y);
+
+            // Fill area
+            for (int y = 1; y < h; y++)
+            {
+                for (int x = 1; x < w; x++)
+                {
+                    var hx = result[x - 1, y] + GetDX(slopes, y, x - 1, x);
+                    var hy = result[x, y - 1] + GetDY(slopes, x, y - 1, y);
+                    result[x, y] = (hx + hy) * .5f;
+                }
+            }
+
+            return result;
+        }
+        public static ArrayTextureMap<float> GenerateHeightMapCross(ArrayTextureMap<Slope> slopes, int startX, int startY)
+        {
+            static float GetDX(ArrayTextureMap<Slope> slopes, int y, int x1, int x2)
+            {
+                var stride = y * slopes.Width;
+                return (slopes[stride + x1].dx + slopes[stride + x2].dx) * 0.5f;
+            }
+            static float GetDY(ArrayTextureMap<Slope> slopes, int x, int y1, int y2)
+            {
+                return (slopes[y1 * slopes.Width + x].dy + slopes[y2 * slopes.Width + x].dy) * 0.5f;
+            }
+
+            var w = slopes.Width;
+            var h = slopes.Height;
+
+            var result = new float[slopes.Count].AsTextureMap(slopes.Width);
+
+            // Draw the cross
+            for (var x = startX + 1; x < w; x++)
+                result[x, startY] = result[x - 1, startY] + GetDX(slopes, startY, x - 1, x);
+            for (var x = startX - 1; x >= 0; x--)
+                result[x, startY] = result[x + 1, startY] + GetDX(slopes, startY, x + 1, x);
+            for (var y = startY + 1; y < h; y++)
+                result[startX, y] = result[startX, y - 1] + GetDY(slopes, startX, y - 1, y);
+            for (var y = startY - 1; y >= 0; y--)
+                result[startX, y] = result[startX, y + 1] + GetDY(slopes, startX, y + 1, y);
+
+            // TODO:
+
+            return result;
+        }
     }
 
     public readonly struct Slope
@@ -50,7 +128,7 @@ namespace DS3TexUpUI
         public static Slope FromNormal(Normal n)
         {
             var f = -1f / (n.Z < 0.001f ? 0.001f : n.Z);
-            return new Slope(n.X * f, n.Y * f);
+            return new Slope(-n.X * f, n.Y * f);
         }
 
         public Normal ToNormal()
