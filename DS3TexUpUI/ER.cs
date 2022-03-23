38d9730acac2041110e9683a9f72ad0b98edf027
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-03-23 22:52:25 +0100

    Added Elden Ring reuse data

---
+++ DS3TexUpUI/ER.cs
@@ -0,0 +1,314 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.IO;
+using SoulsFormats;
+using SixLabors.ImageSharp;
+using Pfim;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Numerics;
+
+#nullable enable
+
+namespace DS3TexUpUI
+{
+    internal static class ER
+    {
+        public const string ExtractDir = @"C:\DS3TexUp\extract-er";
+        public const string DataDir = @"C:\Users\micha\Git\DS3TexUp\er-data";
+
+        public static void ExtractER(IProgressToken token)
+        {
+            const string GameDir = @"C:\Program Files (x86)\Steam\steamapps\common\ELDEN RING\Game";
+
+            Directory.CreateDirectory(ExtractDir);
+
+            void ExtractAsset(SubProgressToken token)
+            {
+                token.SubmitStatus("asset");
+
+                var assetDir = Path.Join(GameDir, "asset", "aet");
+
+                var tpf = Directory
+                    .GetFiles(assetDir, "*.tpf.dcx", SearchOption.AllDirectories)
+                    .Where(f => !f.EndsWith("_l.tpf.dcx"))
+                    .ToArray();
+
+                token.SubmitStatus("Unpacking asset");
+                Yabber.RunParallel(token.Reserve(0.5), tpf);
+
+                token.SubmitStatus("Copying asset");
+                var dds = Directory.GetFiles(assetDir, "*.dds", SearchOption.AllDirectories);
+                var targetDir = Path.Join(ExtractDir, "asset");
+                Directory.CreateDirectory(targetDir);
+
+                token.ForAllParallel(dds, f =>
+                {
+                    var target = Path.Join(targetDir, Path.GetFileName(f));
+                    File.Copy(f, target);
+                });
+            }
+            void ExtractChr(SubProgressToken token)
+            {
+                token.SubmitStatus("chr");
+
+                var chrDir = Path.Join(GameDir, "chr");
+
+                var texbnd = Directory
+                    .GetFiles(chrDir, "*.texbnd.dcx", SearchOption.AllDirectories)
+                    .Where(f => !f.EndsWith("_l.texbnd.dcx"))
+                    .ToArray();
+
+                token.SubmitStatus("Unpacking chr texbnd");
+                Yabber.RunParallel(token.Reserve(0.5), texbnd);
+
+                token.SubmitStatus("Unpacking chr tpf");
+                var tpf = Directory.GetFiles(chrDir, "*.tpf", SearchOption.AllDirectories);
+                Yabber.RunParallel(token.Reserve(0.5), tpf);
+
+                token.SubmitStatus("Copying chr");
+                var dds = Directory.GetFiles(chrDir, "*.dds", SearchOption.AllDirectories);
+                var targetDir = Path.Join(ExtractDir, "chr");
+                Directory.CreateDirectory(targetDir);
+
+                token.ForAllParallel(dds, f =>
+                {
+                    // c0000
+                    var cId = Path.GetFileName(Path.GetDirectoryName(f))!.Substring(0, 5);
+                    var target = Path.Join(targetDir, $"{cId}-{Path.GetFileName(f)}");
+                    File.Copy(f, target);
+                });
+            }
+            void ExtractParts(SubProgressToken token)
+            {
+                token.SubmitStatus("parts");
+
+                var partsDir = Path.Join(GameDir, "parts");
+
+                var partsbnd = Directory
+                    .GetFiles(partsDir, "*.partsbnd.dcx", SearchOption.AllDirectories)
+                    .Where(f => !f.EndsWith("_l.partsbnd.dcx"))
+                    .ToArray();
+
+                token.SubmitStatus("Unpacking parts partsbnd");
+                Yabber.RunParallel(token.Reserve(0.5), partsbnd);
+
+                token.SubmitStatus("Unpacking parts tpf");
+                var tpf = Directory.GetFiles(partsDir, "*.tpf", SearchOption.AllDirectories);
+                Yabber.RunParallel(token.Reserve(0.5), tpf);
+
+                token.SubmitStatus("Copying parts");
+                var dds = Directory.GetFiles(partsDir, "*.dds", SearchOption.AllDirectories);
+                var targetDir = Path.Join(ExtractDir, "parts");
+                Directory.CreateDirectory(targetDir);
+
+                token.ForAllParallel(dds, f =>
+                {
+                    var id = Path.GetFileName(Path.GetDirectoryName(f))!;
+                    id = id.Substring(0, id.Length - "-tpf".Length);
+                    var target = Path.Join(targetDir, $"{id}-{Path.GetFileName(f)}");
+                    File.Copy(f, target);
+                });
+            }
+            void ExtractSfx(SubProgressToken token)
+            {
+                token.SubmitStatus("sfx");
+
+                var sfxDir = Path.Join(GameDir, "sfx");
+
+                var ffxbnd = Directory
+                    .GetFiles(sfxDir, "*.ffxbnd.dcx", SearchOption.AllDirectories)
+                    .Where(f => !f.EndsWith("_l.ffxbnd.dcx"))
+                    .ToArray();
+
+                token.SubmitStatus("Unpacking sfx ffxbnd");
+                Yabber.RunParallel(token.Reserve(0.5), ffxbnd);
+
+                token.SubmitStatus("Unpacking sfx tpf");
+                var tpf = Directory.GetFiles(sfxDir, "*.tpf", SearchOption.AllDirectories);
+                Yabber.RunParallel(token.Reserve(0.5), tpf);
+
+                token.SubmitStatus("Copying sfx");
+                var dds = Directory.GetFiles(sfxDir, "*.dds", SearchOption.AllDirectories);
+                var targetDir = Path.Join(ExtractDir, "sfx");
+                Directory.CreateDirectory(targetDir);
+
+                token.ForAllParallel(dds, f =>
+                {
+                    string p = f;
+                    while (Path.GetFileName(p) != "GR")
+                        p = Path.GetDirectoryName(p)!;
+                    var name = Path.GetFileName(Path.GetDirectoryName(p))!;
+                    name = name.Substring("sfxbnd_".Length).Substring(0, name.Length - "-ffxbnd-dcx".Length);
+                    var target = Path.Join(targetDir, $"{name}-{Path.GetFileName(f)}");
+                    File.Copy(f, target);
+                });
+            }
+
+            token.SplitEqually(
+                ExtractAsset,
+                ExtractChr,
+                ExtractParts,
+                ExtractSfx
+            );
+        }
+        public static void RemoveSolidColor(IProgressToken token)
+        {
+            token.SubmitStatus("Gathering files");
+
+            var dds = Directory.GetFiles(ExtractDir, "*.dds", SearchOption.AllDirectories);
+
+            token.ForAllParallel(dds, file =>
+            {
+                try
+                {
+                    using var image = DDSImage.Load(file);
+                    var diff = image.GetMaxAbsDiff();
+                    if (diff.R <= 10 && diff.G <= 10 && diff.B <= 10 && diff.A <= 10)
+                    {
+                        File.Delete(file);
+                    }
+                }
+                catch (System.Exception e)
+                {
+                    token.LogException(e);
+                }
+            });
+        }
+
+        public static IReadOnlyDictionary<string, TransparencyKind> Alpha
+            = Path.Join(DataDir, "alpha.json").LoadJsonFile<Dictionary<string, TransparencyKind>>();
+        public static IReadOnlyDictionary<string, Size> OriginalSize
+            = Path.Join(DataDir, "size.json").LoadJsonFile<Dictionary<string, Size>>();
+
+        public static ExternalReuse GeneralReuse = new ExternalReuse()
+        {
+            CertainFile = Path.Join(DataDir, "copy-general.json"),
+            UncertainFile = Path.Join(DataDir, "copy-general-uncertain.json"),
+            RejectedFile = Path.Join(DataDir, "copy-general-rejected.json"),
+
+            ExternalDir = ExtractDir,
+            ExternalSize = OriginalSize,
+
+            Ds3Filter = id =>
+            {
+                if (id.GetRepresentative() != id) return false;
+                if (id.IsSolidColor()) return false;
+                if (id.GetTexKind() == TexKind.Normal) return false;
+                return true;
+            },
+            ExternalFilter = file => TexId.GuessTexKind(Path.GetFileNameWithoutExtension(file)) != TexKind.Normal,
+
+            RequireGreater = true,
+            SameKind = true,
+
+            CopySpread = image => image.Count <= 64 * 64 ? 10 : image.Count <= 128 * 128 ? 8 : 6,
+            MaxDiff = new Rgba32(2, 2, 2, 2),
+        };
+
+        public static ExternalReuse AlphaReuse = new ExternalReuse()
+        {
+            CertainFile = Path.Join(DataDir, "copy-alpha.json"),
+            UncertainFile = Path.Join(DataDir, "copy-alpha-uncertain.json"),
+            RejectedFile = Path.Join(DataDir, "copy-alpha-rejected.json"),
+
+            ExternalDir = ExtractDir,
+            ExternalSize = OriginalSize,
+
+            Ds3Filter = id =>
+            {
+                if (id != id.GetAlphaRepresentative()) return false;
+                if (id.IsSolidColor()) return false;
+                var t = id.GetTransparency();
+                if (t != TransparencyKind.Binary && t != TransparencyKind.Full) return false;
+
+                return true;
+            },
+            ExternalFilter = file =>
+            {
+                var t = Alpha.GetOrDefault(file, TransparencyKind.None);
+                if (t != TransparencyKind.Binary && t != TransparencyKind.Full) return false;
+
+                return true;
+            },
+
+            CopyHasherFactory = r => new AlphaImageHasher(r),
+            CopySpread = image => image.Count <= 64 * 64 ? 4 : 2,
+            MaxDiff = new Rgba32(255, 255, 255, 2),
+            ModifyImage = image =>
+            {
+                foreach (ref var p in image.Data.AsSpan())
+                {
+                    p.R = p.A;
+                    p.G = p.A;
+                    p.B = p.A;
+                    p.A = 255;
+                }
+            },
+        };
+
+        public static ExternalReuse NormalReuse = new ExternalReuse()
+        {
+            CertainFile = Path.Join(DataDir, "copy-normal.json"),
+            UncertainFile = Path.Join(DataDir, "copy-normal-uncertain.json"),
+            RejectedFile = Path.Join(DataDir, "copy-normal-rejected.json"),
+
+            ExternalDir = ExtractDir,
+            ExternalSize = OriginalSize,
+
+            Ds3Filter = id =>
+            {
+                if (id.GetNormalRepresentative() != id) return false;
+                if (id.IsSolidColor()) return false;
+                if (id.GetTexKind() != TexKind.Normal) return false;
+                return true;
+            },
+            ExternalFilter = file => TexId.GuessTexKind(Path.GetFileNameWithoutExtension(file)) == TexKind.Normal,
+
+            CopyHasherFactory = r => new NormalImageHasher(r),
+            CopySpread = image => image.Count <= 64 * 64 ? 6 : image.Count <= 128 * 128 ? 4 : 3,
+            MaxDiff = new Rgba32(2, 2, 255, 255),
+            ModifyImage = image =>
+            {
+                image.Multiply(new Rgba32(255, 255, 0, 0));
+                image.SetAlpha(255);
+            },
+        };
+
+        public static ExternalReuse GlossReuse = new ExternalReuse()
+        {
+            CertainFile = Path.Join(DataDir, "copy-gloss.json"),
+            UncertainFile = Path.Join(DataDir, "copy-gloss-uncertain.json"),
+            RejectedFile = Path.Join(DataDir, "copy-gloss-rejected.json"),
+
+            ExternalDir = ExtractDir,
+            ExternalSize = OriginalSize,
+
+            Ds3Filter = id =>
+            {
+                if (id.GetGlossRepresentative() != id) return false;
+                if (id.IsSolidColor()) return false;
+                if (id.GetTexKind() != TexKind.Normal) return false;
+                // ignore all gloss maps that are just solid colors
+                if (DS3.OriginalColorDiff[id].B <= 12) return false;
+                return true;
+            },
+            ExternalFilter = file => TexId.GuessTexKind(Path.GetFileNameWithoutExtension(file)) == TexKind.Normal,
+
+            CopyHasherFactory = r => new BlueChannelImageHasher(r),
+            CopySpread = image => image.Count <= 64 * 64 ? 8 : image.Count <= 128 * 128 ? 5 : 3,
+            MaxDiff = new Rgba32(255, 255, 8, 255),
+            ModifyImage = image =>
+            {
+                foreach (ref var p in image.Data.AsSpan())
+                {
+                    p.R = p.B;
+                    p.G = p.B;
+                    p.A = 255;
+                }
+            },
+        };
+    }
+}
---
+++ DS3TexUpUI/ExternalReuse.cs
@@ -0,0 +1,340 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.IO;
+using SoulsFormats;
+using SixLabors.ImageSharp;
+using Pfim;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Numerics;
+
+#nullable enable
+
+namespace DS3TexUpUI
+{
+    public class ExternalReuse
+    {
+        public string CertainFile { get; set; } = "";
+        public string UncertainFile { get; set; } = "";
+        public string RejectedFile { get; set; } = "";
+
+        public string ExternalDir { get; set; } = "";
+        public IReadOnlyDictionary<string, Size> ExternalSize { get; set; } = new Dictionary<string, Size>();
+
+        public Func<TexId, bool> Ds3Filter { get; set; } = id => true;
+        public Func<string, bool> ExternalFilter { get; set; } = file => true;
+
+        public bool RequireGreater { get; set; } = false;
+        public bool SameKind { get; set; } = false;
+        public Func<SizeRatio, IImageHasher>? CopyHasherFactory { get; set; } = null;
+        public Func<ArrayTextureMap<Rgba32>, int> CopySpread { get; set; } = image => 2;
+        public Rgba32 MaxDiff { get; set; } = default;
+        public Action<ArrayTextureMap<Rgba32>>? ModifyImage { get; set; } = null;
+
+        private Lazy<Dictionary<string, string>> ExternalFiles;
+
+        public ExternalReuse()
+        {
+            ExternalFiles = new Lazy<Dictionary<string, string>>(() =>
+            {
+                using var md5 = System.Security.Cryptography.MD5.Create();
+
+                return Directory.GetFiles(ExternalDir, "*.dds", SearchOption.AllDirectories).ToDictionary(f => f, f =>
+                {
+                    var hash = md5.ComputeHash(System.Text.Encoding.UTF8.GetBytes(f));
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                });
+            });
+        }
+
+        private string GetUncertainFileName(string file)
+        {
+            var hash = ExternalFiles.Value[file];
+            return $"{hash}@{GetExternalSize(file).Width}px.png";
+        }
+        private string ParseUncertainFileName(string path)
+        {
+            var name = Path.GetFileNameWithoutExtension(path);
+            var i = name.IndexOf('@');
+            if (i != -1) name = name.Substring(0, i);
+
+            return ExternalFiles.Value.Where(kv => kv.Value == name).Single().Key;
+        }
+
+        private static string GetUncertainDir(Workspace w) => Path.Join(w.TextureDir, "uncertain");
+
+        private Dictionary<TexId, HashSet<string>> LoadCertain()
+        {
+            return CertainFile.LoadJsonFile<Dictionary<TexId, HashSet<string>>>();
+        }
+        private Dictionary<TexId, HashSet<string>> LoadUncertain()
+        {
+            return Load<Dictionary<TexId, HashSet<string>>>(UncertainFile);
+        }
+        private Dictionary<TexId, HashSet<string>> LoadRejected()
+        {
+            return Load<Dictionary<TexId, HashSet<string>>>(RejectedFile);
+        }
+        private static T Load<T>(string file)
+            where T : new()
+        {
+            if (!File.Exists(file)) return new T();
+            return file.LoadJsonFile<T>();
+        }
+
+        private Size GetExternalSize(string file) => ExternalSize.GetOrDefault(file, Size.Empty);
+
+        public Action<SubProgressToken> CreateCopyIndex(Workspace w)
+        {
+            return token =>
+            {
+                token.SubmitStatus("Searching for files");
+                var files = ExternalFiles.Value.Keys.Where(ExternalFilter).ToArray();
+
+                var index = CopyIndex.Create(token.Reserve(0.5), files, CopyHasherFactory);
+
+                var copies = new Dictionary<TexId, List<string>>();
+
+                token.SubmitStatus($"Looking up files");
+                token.ForAllParallel(DS3.OriginalSize.Keys.Where(Ds3Filter), id =>
+                {
+                    var width = DS3.OriginalSize[id].Width;
+                    var set = new HashSet<string>();
+
+                    try
+                    {
+                        var image = w.GetExtractPath(id).LoadTextureMap();
+                        var similar = index.GetSimilar(image, (byte)CopySpread(image));
+                        if (similar != null)
+                        {
+                            set.UnionWith(similar.Where(e => RequireGreater ? e.Width > width : e.Width >= width).Select(e => e.File));
+                        }
+                    }
+                    catch (System.Exception e)
+                    {
+                        lock (token.Lock)
+                        {
+                            token.SubmitLog($"Ignoring {id} due to error");
+                            token.LogException(e);
+                        }
+                    }
+
+                    if (SameKind)
+                    {
+                        var kind = id.GetTexKind();
+                        set.RemoveWhere(f => TexId.GuessTexKind(Path.GetFileNameWithoutExtension(f)) != kind);
+                    }
+
+                    var list = set.ToList();
+                    list.Sort();
+
+                    if (list.Count > 0)
+                    {
+                        lock (copies)
+                        {
+                            copies[id] = list;
+                        }
+                    }
+                });
+
+                token.SubmitStatus("Saving JSON");
+                copies.SaveAsJson(UncertainFile);
+            };
+        }
+
+        public Action<SubProgressToken> CreateIdentical(Workspace w)
+        {
+            return token =>
+            {
+                token.SubmitStatus("Finding identical");
+                var identical = new Dictionary<TexId, List<string>>();
+
+                bool AreIdentical(ArrayTextureMap<Rgba32> imageA, ArrayTextureMap<Rgba32> imageB)
+                {
+                    var maxDiff = MaxDiff;
+                    for (int i = 0; i < imageA.Count; i++)
+                    {
+                        var pa = imageA[i];
+                        var pb = imageB[i];
+
+                        var diffR = Math.Abs(imageA[i].R - imageB[i].R);
+                        var diffG = Math.Abs(imageA[i].G - imageB[i].G);
+                        var diffB = Math.Abs(imageA[i].B - imageB[i].B);
+                        var diffA = Math.Abs(imageA[i].A - imageB[i].A);
+
+                        if (diffR > maxDiff.R || diffG > maxDiff.G || diffB > maxDiff.B || diffA > maxDiff.A)
+                            return false;
+                    }
+                    return true;
+                }
+
+                token.ForAllParallel(LoadUncertain(), entry =>
+                {
+                    var (id, copies) = entry;
+                    var idSize = DS3.OriginalSize[id];
+                    var image = new Lazy<ArrayTextureMap<Rgba32>>(() => w.GetExtractPath(id).LoadTextureMap());
+
+                    var list = new List<string>();
+                    foreach (var file in copies)
+                    {
+                        var fileSize = GetExternalSize(file);
+                        if (fileSize == idSize && AreIdentical(file.LoadTextureMap(), image.Value))
+                        {
+                            list.Add(file);
+                        }
+                    }
+
+                    list.Sort();
+                    if (list.Count > 0)
+                    {
+                        lock (identical)
+                        {
+                            identical[id] = list;
+                        }
+                    }
+                });
+
+                token.SubmitStatus("Saving JSON");
+                identical.SaveAsJson(CertainFile);
+            };
+        }
+
+        public Action<SubProgressToken> CreateUncertainDirectory(Workspace w)
+        {
+            return token =>
+            {
+                var uncertainDir = GetUncertainDir(w);
+
+                var certain = LoadCertain();
+                var uncertain = LoadUncertain();
+                var rejected = LoadRejected();
+
+                token.SubmitStatus("Copying files");
+                token.ForAllParallel(uncertain, pair =>
+                {
+                    var (id, copies) = pair;
+
+                    var certainFiles = certain.GetOrNew(id);
+                    var rejectedFiles = rejected.GetOrNew(id);
+
+                    // Remove duplicates and rejected
+                    var equal = copies.Except(rejectedFiles).Except(certainFiles).ToList();
+
+                    if (equal.Count == 0) return;
+
+                    if (equal.Count > 25)
+                    {
+                        token.SubmitLog($"Too many copies ({equal.Count}) for {id}.");
+                        return;
+                    }
+
+                    var dir = Path.Join(uncertainDir, id.Category, id.Name);
+                    Directory.CreateDirectory(dir);
+
+                    var maxWidth = equal.Select(f => GetExternalSize(f).Width).Append(DS3.OriginalSize[id].Width).Max();
+
+                    {
+                        var idImage = w.GetExtractPath(id).LoadTextureMap();
+                        var target = Path.Join(dir, $"_@{idImage.Width}px.png");
+                        ModifyImage?.Invoke(idImage);
+                        if (idImage.Width < maxWidth) idImage = idImage.UpSample(maxWidth / idImage.Width);
+                        idImage.SaveAsPng(target);
+                    }
+
+                    foreach (var f in equal)
+                    {
+                        token.CheckCanceled();
+
+                        var image = f.LoadTextureMap();
+                        ModifyImage?.Invoke(image);
+                        if (image.Width < maxWidth) image = image.UpSample(maxWidth / image.Width);
+                        image.SaveAsPng(Path.Join(dir, GetUncertainFileName(f)));
+                    }
+                });
+            };
+        }
+        public Action<SubProgressToken> ReadUncertainDirectory(Workspace w)
+        {
+            return token =>
+            {
+                var uncertainDir = GetUncertainDir(w);
+
+                var files = Directory
+                    .GetFiles(uncertainDir, "*", SearchOption.AllDirectories)
+                    .Where(f => !Path.GetFileName(f).StartsWith("_"))
+                    .ToArray();
+
+                static TexId GetTexIdFromDirectories(string file)
+                {
+                    var p = Path.GetDirectoryName(file);
+                    var name = Path.GetFileName(p);
+                    p = Path.GetDirectoryName(p);
+                    var category = Path.GetFileName(p);
+                    return new TexId(category, name);
+                }
+
+                var oldCertain = LoadCertain();
+                var newCertain = new Dictionary<TexId, List<string>>();
+                foreach (var group in files.GroupBy(GetTexIdFromDirectories))
+                {
+                    var id = group.Key;
+                    var copies = group.Select(ParseUncertainFileName).Union(oldCertain.GetOrNew(id)).ToHashSet().ToList();
+                    copies.Sort();
+                    newCertain[id] = copies;
+                }
+                foreach (var id in oldCertain.Keys.Except(newCertain.Keys).ToList())
+                {
+                    var l = oldCertain[id].ToList();
+                    l.Sort();
+                    newCertain[id] = l;
+                }
+                newCertain.SaveAsJson(CertainFile);
+
+                var rejected = new Dictionary<TexId, List<string>>();
+                foreach (var (id, copies) in LoadUncertain())
+                {
+                    var r = copies.Except(newCertain.GetOrNew(id)).ToList();
+                    r.Sort();
+                    if (r.Count > 0)
+                    {
+                        rejected[id] = r;
+                    }
+                }
+                rejected.SaveAsJson(RejectedFile);
+            };
+        }
+
+        public Action<SubProgressToken> UpdateRejected()
+        {
+            return token =>
+            {
+                var certain = LoadCertain();
+                var rejected = new Dictionary<TexId, List<string>>();
+                foreach (var (id, copies) in LoadUncertain())
+                {
+                    var r = copies.Except(certain.GetOrNew(id)).ToList();
+                    r.Sort();
+                    if (r.Count > 0)
+                    {
+                        rejected[id] = r;
+                    }
+                }
+                rejected.SaveAsJson(RejectedFile);
+            };
+        }
+
+        public Action<SubProgressToken> ManuallyMakeEqual(EquivalenceCollection<TexId> certain)
+        {
+            return token =>
+            {
+                // certain.Set(LoadCertain());
+                // certain.SaveAsJson(CertainFile);
+
+                // var rejected = DifferenceCollection<TexId>.FromUncertain(LoadUncertain(), certain);
+                // rejected.SaveAsJson(RejectedFile);
+            };
+        }
+    }
+}
