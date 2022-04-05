548fdf998e87651ecd94e2d1ca7f8b27202ea55c
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-04-05 22:22:53 +0200

    Added albedo training

--- DS3TexUpUI/Training.cs
+++ DS3TexUpUI/Training.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using Pfim;
 using SixLabors.ImageSharp.PixelFormats;
@@ -9,8 +10,48 @@ namespace DS3TexUpUI
 {
     internal static class Training
     {
+        public static void UnzipColorData(IProgressToken token)
+        {
+            const string ZipDir = @"D:\PBR materials";
+            const string TargetDir = @"C:\Users\micha\Desktop\train\raw-data-wood";
 
-        public static void UnzipData(IProgressToken token)
+            var zips = Directory.GetFiles(ZipDir, "*.zip", SearchOption.AllDirectories);
+
+            token.ForAllParallel(zips, zip =>
+            {
+                var name = Path.GetFileName(zip);
+                var category = Path.GetFileName(Path.GetDirectoryName(zip));
+
+                using var archive = ZipFile.OpenRead(zip);
+                var albedo = archive.Entries.Where(e =>
+                {
+                    var name = Path.GetFileNameWithoutExtension(e.Name).ToLowerInvariant();
+                    return name.EndsWith("color") || name.EndsWith("albedo");
+                }).ToList();
+
+                if (albedo.Count != 1)
+                {
+                    token.SubmitLog($"Cannot extract albedo from {category}/{name}. Found {albedo.Count} albedos.");
+                    // return;
+                }
+
+                foreach (var a in albedo)
+                {
+                    var target = Path.Join(TargetDir, category, a.Name);
+                    Directory.CreateDirectory(Path.GetDirectoryName(target));
+
+                    var temp = target + ".temp";
+                    using (var zipS = a.Open())
+                    using (var fs = File.Create(temp))
+                    {
+                        zipS.CopyTo(fs);
+                    }
+
+                    File.Move(temp, target);
+                }
+            });
+        }
+        public static void UnzipNormalData(IProgressToken token)
         {
             const string ZipDir = @"C:\Users\micha\Desktop\train\zip";
             const string TempDir = @"C:\Users\micha\Desktop\train\tem";
@@ -69,9 +110,103 @@ namespace DS3TexUpUI
         const int HRSize = 512;
         const int LRSize = HRSize / 4;
 
-        public static void CreateHR(IProgressToken token, string inputDir, string outputDir)
+        public static void CreateAlbedoHR(IProgressToken token, string inputDir, string outputDir)
         {
+            Directory.CreateDirectory(outputDir);
+            var images = Directory.GetFiles(inputDir, "*.png", SearchOption.AllDirectories);
+            Array.Sort(images);
 
+            token.ForAllParallel(images.Select((x, i) => (x, i)), p =>
+            {
+                var (imageFile, id) = p;
+
+                var image = imageFile.LoadTextureMap();
+                image.SetAlpha(255);
+
+                if (image.Width % HRSize != 0 || image.Height % HRSize != 0 || !image.Width.IsPowerOfTwo() || !image.Height.IsPowerOfTwo())
+                {
+                    token.SubmitLog($"Cannot processes {imageFile} because of its size.");
+                    return;
+                }
+
+                var r = new Random(id);
+
+                while (image.Width >= HRSize && image.Height >= HRSize)
+                {
+                    var totalCuts = image.Width / HRSize * image.Height / HRSize;
+                    var maxCount = 16;
+                    var chance = Math.Min(1.0, maxCount / (double)totalCuts);
+                    for (int x = 0; x < image.Width; x += HRSize)
+                    {
+                        for (int y = 0; y < image.Height; y += HRSize)
+                        {
+                            if (r.NextDouble() > chance) continue;
+
+                            token.CheckCanceled();
+                            var cut = image.GetCut(x, y, HRSize, HRSize);
+                            cut.SaveAsPng(Path.Join(outputDir, $"i{id}-{image.Width}-tile-{x}-{y}-{HRSize}.png"));
+                        }
+                    }
+
+                    image = image.DownSample(Average.Rgba32GammaAlpha, 2);
+                }
+            });
+        }
+        public static void CreateAlbedoLR(IProgressToken token, string inputDir, string outputDir, LRCompression compression)
+        {
+            var tempDir = Path.Join(outputDir, "temp");
+            Directory.CreateDirectory(tempDir);
+
+            var images = Directory.GetFiles(inputDir, "*.png", SearchOption.AllDirectories);
+            Random random = new Random();
+
+            try
+            {
+                token.ForAllParallel(images, imageFile =>
+                {
+                    var name = Path.GetFileName(imageFile);
+                    var target = Path.Join(outputDir, name);
+
+                    var image = imageFile.LoadTextureMap();
+
+                    var small = random.NextDouble() < 0.1
+                        ? image.DownSample(Average.Rgba32, 4)
+                        : image.DownSample(Average.Rgba32GammaAlpha, 4);
+
+                    if (compression == LRCompression.Uncompressed)
+                    {
+                        small.SaveAsPng(target);
+                    }
+                    else
+                    {
+                        var tempPng = Path.Join(tempDir, name);
+                        var tempDds = Path.ChangeExtension(tempPng, ".dds");
+                        small.SaveAsPng(tempPng);
+
+                        DDSExtensions.ToDDSUsingTexConv(
+                            tempPng,
+                            tempDds,
+                            format: compression == LRCompression.BC1 ? DxgiFormat.BC1_UNORM : DxgiFormat.BC7_UNORM,
+                            dithering: random.NextDouble() < 0.2
+                        );
+
+                        var dds = tempDds.LoadTextureMap();
+                        dds.Multiply(new Rgba32(255, 255, 0, 255));
+                        dds.SaveAsPng(target);
+
+                        File.Delete(tempPng);
+                        File.Delete(tempDds);
+                    }
+                });
+            }
+            finally
+            {
+                Directory.Delete(tempDir, true);
+            }
+        }
+
+        public static void CreateNormalHR(IProgressToken token, string inputDir, string outputDir)
+        {
             Directory.CreateDirectory(outputDir);
             var images = Directory.GetFiles(inputDir, "*.png", SearchOption.AllDirectories);
             Array.Sort(images);
@@ -113,7 +248,7 @@ namespace DS3TexUpUI
                 }
             });
         }
-        public static void CreateLR(IProgressToken token, string inputDir, string outputDir, LRCompression compression)
+        public static void CreateNormalLR(IProgressToken token, string inputDir, string outputDir, LRCompression compression)
         {
             var tempDir = Path.Join(outputDir, "temp");
             Directory.CreateDirectory(tempDir);
