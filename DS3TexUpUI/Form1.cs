db36bdfcb8aa68688da0ac73d4461bac6332ef59
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-06-02 16:22:24 +0200

    Improved unused manual overrides reporting

--- DS3TexUpUI/Form1.cs
+++ DS3TexUpUI/Form1.cs
@@ -233,41 +233,32 @@ namespace DS3TexUpUI
         {
             var p = GetProject();
 
-            var unused = new List<string>();
-            void AddUnused(IEnumerable<KeyValuePair<TexId, string>> files)
+            var unused = new List<(string type, string file, TexId id, TexId representative)>();
+            void AddUnused(string type, TexOverrideList textures, Func<TexId, TexId> getRepresentative)
             {
-                unused.AddRange(files.Select(kv => kv.Value));
+                token.SubmitStatus(type);
+                foreach (var (id, file) in textures.GetFiles())
+                {
+                    if (!file.StartsWith(@"C:\DS3TexUp\up-manual\")) continue;
+
+                    var r = getRepresentative(id);
+                    if (r != id)
+                    {
+                        unused.Add((type, file, id, r));
+                        token.SubmitLog($"Unused {type} override: {id} ({file}) is represented by {r}");
+                    }
+                }
             }
 
-            token.SubmitStatus("Albedo");
-            AddUnused(p.Textures.Albedo.GetFiles().Where(kv => kv.Key.GetRepresentative() != kv.Key));
+            AddUnused("Albedo", p.Textures.Albedo, id => id.GetRepresentative());
+            AddUnused("Alpha", p.Textures.Alpha, id => id.GetAlphaRepresentative());
+            AddUnused("Normal normal", p.Textures.NormalNormal, id => id.GetNormalRepresentative());
+            AddUnused("Normal gloss", p.Textures.NormalGloss, id => id.GetGlossRepresentative());
+            AddUnused("Reflective", p.Textures.Reflective, id => id.GetRepresentative());
+            AddUnused("Shininess", p.Textures.Shininess, id => id.GetRepresentative());
+            AddUnused("Emissive", p.Textures.Emissive, id => id.GetRepresentative());
 
-            token.SubmitStatus("Alpha");
-            AddUnused(p.Textures.Alpha.GetFiles().Where(kv => kv.Key.GetAlphaRepresentative() != kv.Key));
-
-            token.SubmitStatus("Normal normal");
-            AddUnused(p.Textures.NormalNormal.GetFiles().Where(kv => kv.Key.GetNormalRepresentative() != kv.Key));
-
-            token.SubmitStatus("Normal gloss");
-            AddUnused(p.Textures.NormalGloss.GetFiles().Where(kv => kv.Key.GetGlossRepresentative() != kv.Key));
-
-            token.SubmitStatus("Reflective");
-            AddUnused(p.Textures.Reflective.GetFiles().Where(kv => kv.Key.GetRepresentative() != kv.Key));
-            token.SubmitStatus("Shininess");
-            AddUnused(p.Textures.Shininess.GetFiles().Where(kv => kv.Key.GetRepresentative() != kv.Key));
-            token.SubmitStatus("Emissive");
-            AddUnused(p.Textures.Emissive.GetFiles().Where(kv => kv.Key.GetRepresentative() != kv.Key));
-
-            token.SubmitStatus("Filtering manual");
-            var manual = @"C:\DS3TexUp\up-manual\";
-            var unusedManual = unused.Where(f => f.StartsWith(manual)).ToHashSet().ToList();
-            unusedManual.Sort();
-
-            if (unusedManual.Count > 0)
-            {
-                token.SubmitLog($"Found {unusedManual.Count} unused manual texture overrides:");
-                token.SubmitLog(string.Join("\n", unusedManual.Select(s => $"  {TexId.FromPath(s)}  {s}")));
-            }
+            if (unused.Count > 0) token.SubmitLog($"Found {unused.Count} unused manual texture overrides:");
         }
         private void UpdateManualNormalAlbedo(IProgressToken token)
         {
@@ -622,11 +613,43 @@ namespace DS3TexUpUI
             // m37_00_00_00_020151
             var w = GetWorkspace();
 
-            var ids = ParseCurrent().ids;
-            var rep = DS3.RepresentativeOf.GroupBy(kv => kv.Value).ToDictionary(g => g.Key, g => g.Select(kv => kv.Key).ToList());
-            var na = DS3.NormalAlbedo.GroupBy(kv => kv.Value).ToDictionary(g => g.Key, g => g.Select(kv => kv.Key).ToList());
-            ids = ids.SelectMany(id => rep.GetOrNew(id).Append(id)).SelectMany(id => na.GetOrNew(id).Append(id)).ToHashSet();
-            Clipboard.SetText(string.Join("\n", ids));
+            foreach (var f in Directory.GetFiles(@"C:\Users\micha\Desktop\n"))
+            {
+                var image = f.LoadTextureMap();
+                var blurred = image.Blur(3, Average.Rgba32);
+
+                for (int i = 0; i < image.Data.Length; i++)
+                {
+                    var pi = image[i];
+                    var pb = blurred[i];
+                    var ni = Normal.FromRG(pi.R, pi.G);
+                    var nb = Normal.FromRG(pb.R, pb.G);
+                    var (r, g) = Normal.HeightMapAddition(ni, 1, nb, -1).ToRG();
+                    image.Data[i].R = r;
+                    image.Data[i].G = g;
+                }
+
+                blurred = image.Blur(2, Average.Rgba32);
+
+                for (int i = 0; i < image.Data.Length; i++)
+                {
+                    var pi = image[i];
+                    var pb = blurred[i];
+                    var ni = Normal.FromRG(pi.R, pi.G);
+                    var nb = Normal.FromRG(pb.R, pb.G);
+                    var (r, g) = Normal.HeightMapAddition(ni, 1, nb, -1).ToRG();
+                    image.Data[i].R = r;
+                    image.Data[i].G = g;
+                }
+
+                image.SaveAsPng(Path.Join(@"C:\Users\micha\Desktop\n\high-3-2", $"{Path.GetFileNameWithoutExtension(f)}.png"));
+            }
+
+            // var ids = ParseCurrent().ids;
+            // var rep = DS3.RepresentativeOf.GroupBy(kv => kv.Value).ToDictionary(g => g.Key, g => g.Select(kv => kv.Key).ToList());
+            // var na = DS3.NormalAlbedo.GroupBy(kv => kv.Value).ToDictionary(g => g.Key, g => g.Select(kv => kv.Key).ToList());
+            // ids = ids.SelectMany(id => rep.GetOrNew(id).Append(id)).SelectMany(id => na.GetOrNew(id).Append(id)).ToHashSet();
+            // Clipboard.SetText(string.Join("\n", ids));
 
             // var tiles = new Dictionary<TexId, List<Tile>>();
             // foreach (var id in ParseCurrent().ids)
@@ -651,37 +674,15 @@ namespace DS3TexUpUI
 
             // RunTask(token =>
             // {
-            //     var pairs = new (string, string, bool isEqual)[]{
-            //         ("m34/m34_o1240_01_n", "m33/m33_00_o1240_n", true),
-            //         ("m31/m31_00_spiderweb_01_n", "m38/m38_00_m22_00_grass_502_n", false),
-            //         ("m31/m31_00_ruinedge_01_a", "m33/m33_00_ruinedge_01_a", true),
-            //         ("m33/m33_00_ruinedge_01_a", "chr/c1350_c1350_weapon2_a", false),
-            //         ("chr/c2190_c2190_wing_n", "obj/o399910_c2190_wing_n", true),
-            //         ("chr/c1446_c1446_tunic_r", "chr/c1441_c1441_tunic_r", true),
-            //         ("chr/c3080_c3080arms_r", "obj/o302593_o302593_c3080arms_r", true),
-            //         ("m37/m37_00_firebrand_01_r", "m38/m38_00_o9569_r", true),
-            //         ("m41/m41_00_m36_00_Test_branch_tiling_01_r", "chr/c1109_c1109_hood_r", false),
-            //         ("m31/m31_00_spiderweb_03_n", "m30/m30_00_o344883_03_n",false),
-            //         ("m31/m31_00_woodbox_00_n","obj/o001080_o001080_woodenbox_n",true),
-            //         ("m32/m32_00_o324005_railing_n","m40/m40_00_railing_000_n",false),
-            //         ("m32/m32_00_o329222_2_r", "m30/m30_00_o329222_2_rubble_01_r", true),
-            //         ("chr/c6260_c6260_Shield_Small_n","parts/WP_A_2031_WP_A_2031_n",true),
-            //         ("chr/c1070_c1070_tree_n", "m31/m31_00_statue_00_n", true),
-            //         ("chr/c1370_c1370_hair_n", "chr/c1240_c1240_cloths_n", true),
-            //         ("chr/c1102_c1102_shield2_n", "obj/o398720_o398720_c1100_shield2_n", true),
-            //         ("chr/c1070_c1070_wp_a_0404_n", "chr/c1071_c1071_wp_a_0404_n", true),
-            //     };
-
-            //     var result = new List<string>();
-            //     token.ForAll(pairs, pair =>
-            //     {
-            //         var a = w.GetExtractPath(new TexId(pair.Item1)).LoadTextureMap();
-            //         var b = w.GetExtractPath(new TexId(pair.Item2)).LoadTextureMap();
-            //         var sim = a.GetSimilarityScore(b);
-            //         result.Add($"{sim} E:{pair.isEqual} A:{sim.color < 0.04 && sim.feature < 0.12}");
-            //     });
-
-            //     MessageBox.Show(string.Join("\n", result));
+            //     var hr = @"C:\Users\micha\Desktop\train\data6\hr";
+            //     var lr = @"C:\Users\micha\Desktop\train\data6\lr";
+            //     var lrBc1 = @"C:\Users\micha\Desktop\train\data6\lr-bc1";
+            //     var lrBc7 = @"C:\Users\micha\Desktop\train\data6\lr-bc7";
+            //     // Training.CreateHR(token, @"C:\Users\micha\Desktop\train\raw-data", hr);
+            //     // Training.CreateLR(token, hr, lr, Training.LRCompression.Uncompressed);
+            //     // Training.CreateLR(token, hr, lrBc1, Training.LRCompression.BC1);
+            //     Training.CreateLR(token, hr, lrBc7, Training.LRCompression.BC7);
+            //     Training.PickValidation(token, new string[] { hr, lr, lrBc1, lrBc7 }, 0.005);
             // });
         }
 
