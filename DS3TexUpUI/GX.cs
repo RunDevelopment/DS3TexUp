6504f5529ad3c5cd1a4f000c89a1c3d7723d3be9
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-09-27 21:58:19 +0200

    Added GX value types

--- DS3TexUpUI/GX.cs
+++ DS3TexUpUI/GX.cs
@@ -1,35 +1,28 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Globalization;
 using SoulsFormats;
 
 namespace DS3TexUpUI
 {
-    public class GXItemBuilder
+    public static class GX
     {
-        public string ID;
-        public int Unk04;
-        public List<GXValue> Values;
-
-        public GXItemBuilder(string id, int unk04)
+        public static GXIdType ParseGXId(this string id) => ParseGXId(id.AsSpan());
+        public static GXIdType ParseGXId(this ReadOnlySpan<char> id)
         {
-            ID = id;
-            Unk04 = unk04;
-            Values = new List<GXValue>();
-        }
-        public GXItemBuilder(FLVER2.GXItem item)
-        {
-            ID = item.ID;
-            Unk04 = item.Unk04;
-            Values = BytesToValues(item.Data);
+            if (id == "GXMD")
+                return GXIdType.GXMD;
+            if (id.StartsWith("GX") && id.Length == 4 && id[2].IsDigit() && id[3].IsDigit())
+                return GXIdType.GX00;
+            return GXIdType.Unknown;
         }
 
-        public FLVER2.GXItem ToItem()
-        {
-            return new FLVER2.GXItem(ID, Unk04, ValuesToBytes(Values));
-        }
+        public static bool IsGXMD(string id) => id.ParseGXId() == GXIdType.GXMD;
+        public static bool IsGX00(string id) => id.ParseGXId() == GXIdType.GX00;
 
-        private static List<GXValue> BytesToValues(ReadOnlySpan<byte> bytes)
+        public static List<GXValue> ToGxValues(this byte[] bytes) => ToGxValues(bytes.AsSpan());
+        public static List<GXValue> ToGxValues(this ReadOnlySpan<byte> bytes)
         {
             if (bytes.Length % 4 != 0)
                 throw new ArgumentException("The number of bytes has to be divisible by 4.");
@@ -39,13 +32,31 @@ namespace DS3TexUpUI
                 values.Add(BitConverter.ToInt32(bytes.Slice(i, 4)));
             return values;
         }
-        private static byte[] ValuesToBytes(List<GXValue> values)
+        public static byte[] ToGxDataBytes(this IEnumerable<GXValue> values)
         {
             var bytes = new List<byte>();
             foreach (var value in values)
                 bytes.AddRange(value.GetBytes());
             return bytes.ToArray();
         }
+
+        /// <summary>
+        /// A mapping from all known GX00 ids to its Unk04 value.
+        /// </summary>
+        public static IReadOnlyDictionary<string, int> GX00Unk04
+            = Data.File("gx/gx00-unk04").LoadJsonFile<Dictionary<string, int>>();
+        /// <summary>
+        /// All observed Unk04 values for GXMD.
+        /// </summary>
+        public static IReadOnlyList<int> GXMDUnk04
+            = Data.File("gx/gxmd-unk04").LoadJsonFile<List<int>>();
+    }
+
+    public enum GXIdType
+    {
+        GXMD,
+        GX00,
+        Unknown
     }
 
     [StructLayout(LayoutKind.Explicit)]
@@ -67,9 +78,21 @@ namespace DS3TexUpUI
 
         public byte[] GetBytes() => BitConverter.GetBytes(I);
 
+        public override string ToString()
+        {
+            return I.ToString(CultureInfo.InvariantCulture) + "/" + F.ToString(CultureInfo.InvariantCulture);
+        }
+
         public static implicit operator int(GXValue v) => v.I;
         public static implicit operator float(GXValue v) => v.F;
         public static implicit operator GXValue(int i) => new GXValue(i);
         public static implicit operator GXValue(float f) => new GXValue(f);
+    }
+
+    public enum GXValueType
+    {
+        Unknown = 0,
+        Int = 1,
+        Float = 2,
     }
 }
