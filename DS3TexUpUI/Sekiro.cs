d60776e0f61f4e85068b7ddb872cd9175f5ed45d
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-10-01 14:48:36 +0200

    Added Sekiro material info

---
+++ DS3TexUpUI/Sekiro.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.IO;
+using SoulsFormats;
+using SixLabors.ImageSharp;
+using Pfim;
+using SixLabors.ImageSharp.PixelFormats;
+
+#nullable enable
+
+namespace DS3TexUpUI
+{
+    internal static class Sekiro
+    {
+        public static IEnumerable<FlverMaterialInfo> ReadAllFlverMaterialInfo()
+        {
+            foreach (var file in Directory.GetFiles(Data.File(name: @"sk/materials"), "*.json"))
+                foreach (var item in file.LoadJsonFile<List<FlverMaterialInfo>>())
+                    yield return item;
+        }
+    }
+}
