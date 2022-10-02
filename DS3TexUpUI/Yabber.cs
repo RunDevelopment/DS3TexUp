1efb1effdc3c25eb2cce2cb56156f027dcb3bd77
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-10-02 12:55:23 +0200

    Yabber: Don't unpack already unpacked archives

--- DS3TexUpUI/Yabber.cs
+++ DS3TexUpUI/Yabber.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Text;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,26 @@ namespace DS3TexUpUI
 {
     public class Yabber
     {
+        public static string[] Filter(string[] files)
+        {
+            return files
+                .Where(f =>
+                {
+                    var ext = Path.GetExtension(f);
+                    if (ext != null && (ext == ".dcx" || ext.EndsWith("bnd")))
+                    {
+                        // yabber might have decompressed certain archives already,
+                        // in which case we don't have to do it again
+                        var yabberDir = Path.Join(Path.GetDirectoryName(f), Path.GetFileName(f).Replace('.', '-'));
+                        if (Directory.Exists(yabberDir))
+                            return false;
+                    }
+
+                    return true;
+                })
+                .ToArray();
+        }
+
         public static void Run(params string[] files)
         {
             foreach (var chunk in files.Chunks(16))
@@ -16,23 +37,29 @@ namespace DS3TexUpUI
                 RunProcess(chunk);
             }
         }
-        public static void Run(IProgressToken token, params string[] files)
+        public static void Run(IProgressToken token, string[] files, bool filter = true)
         {
+            if (filter) files = Filter(files);
+
             token.ForAll(files.Chunks(16), files.Length, chunk =>
             {
                 RunProcess(chunk);
                 return chunk.Length;
             });
         }
-        public static void RunParallel(string[] files)
+        public static void RunParallel(string[] files, bool filter = true)
         {
+            if (filter) files = Filter(files);
+
             var degreeOfParallelism = AppConfig.Instance.MaxDegreeOfParallelism;
             var chunks = files.Chunks(Math.Min(1, Math.Max(files.Length / degreeOfParallelism, 16)));
 
             Parallel.ForEach(chunks, RunProcess);
         }
-        public static void RunParallel(IProgressToken token, string[] files)
+        public static void RunParallel(IProgressToken token, string[] files, bool filter = true)
         {
+            if (filter) files = Filter(files);
+
             var degreeOfParallelism = AppConfig.Instance.MaxDegreeOfParallelism;
             var chunks = files.Chunks(Math.Min(1, Math.Max(files.Length / degreeOfParallelism, 16)));
 
